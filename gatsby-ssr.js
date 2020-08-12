58496f20e5b0622569a488cf67c89565343d4006
tom <tomphill@live.co.uk>
2020-08-12 16:27:47 +0800

    context ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { ProductContextProvider } from './src/context/ProductContext';
+import { CartContextProvider } from './src/context/CartContext';
 import { GlobalStyle } from './src/components/globalStyles';
+
+export const wrapRootElement = ({ element }) => (
+  <ProductContextProvider>
+    <CartContextProvider>{element}</CartContextProvider>
+  </ProductContextProvider>
+);
 
 export const wrapPageElement = ({ element }) => (
   <>
