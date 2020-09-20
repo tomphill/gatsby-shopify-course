bb0c3455998c38e89a8063d63b1684f928ca7318
tom <tomphill@live.co.uk>
2020-09-20 10:00:41 +0800

    log json body

--- functions/product-update.js
+++ functions/product-update.js
@@ -1,5 +1,5 @@
 exports.handler = function (event, context, callback) {
-  console.log(event);
+  console.log(JSON.parse(event.body));
   console.log(context);
   callback(null, {
     statusCode: 200,
