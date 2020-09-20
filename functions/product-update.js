a05cf00d7a789ce74ffc25c9abf4d682775aaac2
tom <tomphill@live.co.uk>
2020-09-20 10:27:39 +0800

    request verification

--- functions/product-update.js
+++ functions/product-update.js
@@ -1,11 +1,28 @@
+const crypto = require('crypto');
+
 exports.handler = function (event, context, callback) {
   console.log('--------------- BODY ---------------');
   console.log(JSON.parse(event.body));
   console.log('--------------- HEADERS ---------------');
   console.log(event.headers);
-  console.log('--------------- CONTEXT ---------------');
-  console.log(context);
-  callback(null, {
-    statusCode: 200,
-  });
+
+  const hmac = event.headers['x-shopify-hmac-sha256'];
+
+  const hash = crypto
+    .createHmac('sha256', process.env.SHOPIFY_WEBHOOK_KEY)
+    .update(event.body, 'utf8', 'hex')
+    .digest('base64');
+
+  // Compare our hash to Shopify's hash
+  if (hash === hmac) {
+    // It's a match! All good
+    callback(null, {
+      statusCode: 200,
+    });
+  } else {
+    // No match! This request didn't originate from Shopify
+    callback(null, {
+      statusCode: 403,
+    });
+  }
 };
