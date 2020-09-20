a4bcf03f331ff9373d36ea173f132a731f7f5a30
tom <tomphill@live.co.uk>
2020-09-20 10:10:22 +0800

    log headers

--- functions/product-update.js
+++ functions/product-update.js
@@ -1,5 +1,9 @@
 exports.handler = function (event, context, callback) {
+  console.log('--------------- BODY ---------------');
   console.log(JSON.parse(event.body));
+  console.log('--------------- HEADERS ---------------');
+  console.log(event.headers);
+  console.log('--------------- CONTEXT ---------------');
   console.log(context);
   callback(null, {
     statusCode: 200,
